44cb805f25490c0d928cb78bd8f541462366bcb0
Chase Southard <chase.southard@gmail.com>
2012-01-03 10:33:23 -0500

    minor tweaks

--- NewYearShoutOut.rb
+++ NewYearShoutOut.rb
@@ -31,7 +31,7 @@ auth_token = twilio_config['auth_token']
 # an array of peeps
 peeps = [
  # "+18595551212",
- "+18595520123"
+ "+18595551212" #fake number
 ]
 
 # iterate and send an SMS to your peeps
@@ -39,7 +39,7 @@ peeps.each do |e|
 
 @client.account.sms.messages.create(
   #:from => '+14155992671', # twilio sandbox number
-  :from => '+18597953743', # my twilio number
+  :from => twilio_config['number'],
   :to => e,
   :body => "Wishing you Peace and Prosperity in 2012\r\nAlso, HAPPY NEW YEAR!!\r\nsent by Chase\'s SMS Robot"
 )
@@ -51,7 +51,7 @@ end
 # get your twitter cred
 twitter_config = YAML.load_file(File.join(File.expand_path("~"), ".twitter", "keys.yml"))
 
-configure twitter object
+#configure twitter object
 Twitter.configure do |config|
   config.consumer_key = twitter_config['consumer_key']
   config.consumer_secret = twitter_config['consumer_secret']
